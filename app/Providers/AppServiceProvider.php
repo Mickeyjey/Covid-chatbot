6dd242c8d555511825725d6a0975ef0183dad488
Mickeyjey <37717787+Mickeyjey@users.noreply.github.com>
2021-01-06 02:07:52 +0000

    About last update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if (app()->environment('remote')) {
-            URL::forceSchema('https');
+            URL::forceScheme('https');
         }
     }
 
